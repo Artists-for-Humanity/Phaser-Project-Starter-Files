b6c6472a9472691e1a7052e60f6f29c05eb74825
HandyDorceus <dorceus.handy@gmail.com>
2022-03-24 16:04:58 -0400

    converted js file to ts files. Removed images assets.

--- src/scripts/Sprites/Player.js
+++ src/scripts/Sprites/Player.js
@@ -2,13 +2,29 @@ import Phaser from 'phaser';
 
 export default class Player extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y) {
-    super(scene, x, y, 'player');
+    super(scene, x, y, 'spraycan');
 
     scene.add.existing(this);
     scene.physics.world.enableBody(this);
     this.setCollideWorldBounds(true);
-    this.setScale(0.5);
+
+    this.cursors = scene.input.keyboard.createCursorKeys();
 
     return this;
+  }
+
+  update() {
+    if (this.cursors.left.isDown) {
+      this.x -= 10;
+    }
+    if (this.cursors.right.isDown) {
+      this.x += 10;
+    }
+    if (this.cursors.up.isDown) {
+      this.y -= 10;
+    }
+    if (this.cursors.down.isDown) {
+      this.y += 10;
+    }
   }
 }
