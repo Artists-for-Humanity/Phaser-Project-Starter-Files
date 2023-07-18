92d1c70bd1397656e45375b81eb061d4e1c13812
Serenity <swilliams314159@gmail.com>
2023-07-18 17:03:51 -0400

    working on arrow function still in recipe section of code

--- src/scripts/Scenes/ArrozConLeche.ts
+++ src/scripts/Scenes/ArrozConLeche.ts
@@ -21,17 +21,20 @@ export default class ArrozConLeche extends Phaser.Scene {
     this.load.image('EquipmentBar', new URL('../../assets/EquipmentBar.png', import.meta.url).href);
     this.load.image('EquipmentBarO', new URL('../../assets/EquipmentBarO.png', import.meta.url).href);
     this.load.image('I&EOpened', new URL('../../assets/I&EOpened.png', import.meta.url).href);
-    this.load.image('Arrow', new URL('../../assets/Arrow.png', import.meta.url).href);
+    this.load.image('down', new URL('../../assets/DownArrow.png', import.meta.url).href);
+    this.load.image('up', new URL('../../assets/UpArrow.png', import.meta.url).href);
 
     //Assets
-    this.load.image('Rice', new URL('../../assets/Rice.png', import.meta.url).href);
-    this.load.image('Water', new URL('../../assets/Water.png', import.meta.url).href);
-    this.load.image('Cinnamon', new URL('../../assets/Cinnamon.png', import.meta.url).href);
-    this.load.image('CondensedMilk', new URL('../../assets/CondensedMilk.png', import.meta.url).href);
+    this.load.image('Rice', new URL('../../assets/ingred/Rice.png', import.meta.url).href);
+    this.load.image('Water', new URL('../../assets/ingred/Water.png', import.meta.url).href);
+    this.load.image('Cinnamon', new URL('../../assets/ingred/Cinnamon.png', import.meta.url).href);
+    this.load.image('CondensedMilk', new URL('../../assets/ingred/CondensedMilk.png', import.meta.url).href);
     this.load.image('Pot', new URL('../../assets/equip/Pot.png', import.meta.url).href);
     this.load.image('Bowl', new URL('../../assets/equip/Bowl.png', import.meta.url).href);
     this.load.image('WoodenSpoon', new URL('../../assets/equip/WoodenSpoon.png', import.meta.url).href);
     this.load.image('CuttingBoard', new URL('../../assets/equip/CuttingBoard.png', import.meta.url).href);
+    this.load.image('ACL_Recipe', new URL('../../assets/recipes/ACL_Recipe.png', import.meta.url).href);
+    this.load.image('ACL_Instructions', new URL('../../assets/recipes/ACL_Instructions.png', import.meta.url).href);
   }
 
   create() {
@@ -39,7 +42,7 @@ export default class ArrozConLeche extends Phaser.Scene {
     const gameWidth: number = this.game.config.width as number;
     const gameHeight: number = this.game.config.height as number;
     this.add.image(gameWidth / 2, gameHeight / 2, 'Background');
-    
+    var RecipeIndex = 1;
 
     var HomeButton = this.add.image(50, 61, 'HomeButton');
     HomeButton.setInteractive();
@@ -51,17 +54,46 @@ export default class ArrozConLeche extends Phaser.Scene {
 
     RecipeBar.on('pointerup', () => {
       var RBarC = this.add.image(460, 210, 'RecipeOpened'); 
-      var RArrow = this.add.image(650, 210, 'Arrow');
-      RArrow.setInteractive();
-      RArrow.on('pointerup', () => {
-        console.log("r arrow click");
+      var recipe = this.add.image(250, 210, 'ACL_Recipe');
+      var instructions = this.add.image(435, 210, 'ACL_Instructions');
+
+      var RDownArrow = this.add.image(630, 210, 'down');
+      RDownArrow.setInteractive();
+      
+      RDownArrow.on('pointerup', () => {
+        RUpArrow.visible===false ? ()=>{RDownArrow.setVisible(true); RecipeIndex--;}:
+        if (RecipeIndex<3){
+          
+          RecipeIndex++;
+          RecipeIndex===3  ? RDownArrow.setVisible(false):
+          this.cycleRecipe(RecipeIndex);
+        }
+        
+        console.log(RecipeIndex);
         //gimme more recipe
       });
+      var RUpArrow = this.add.image(700, 210, 'up');
+      RUpArrow.setInteractive();
+      RUpArrow.on('pointerup', () => {
+        RDownArrow.visible===false ? ()=>{RDownArrow.setVisible(true); RecipeIndex--;}: 
+        if (RecipeIndex>=1){
+           
+          RecipeIndex--;
+          RecipeIndex===1  ? RUpArrow.setVisible(false):
+          this.cycleRecipe(RecipeIndex);
+        } 
+        
+        console.log(RecipeIndex);
+        //gimme more recipe
+      });
+
       var RBarO = this.add.image(225, 50, 'RecipeBarO');
       RBarO.setInteractive();
       RBarO.on('pointerup', () => {
+        ACLi.setVisible(false);
+        ACLr.setVisible(false);
         RBarC.setVisible(false);
-        RArrow.setVisible(false);
+        RDownArrow.setVisible(false);
         RBarO.setVisible(false);     
       });
     })
@@ -139,11 +171,31 @@ export default class ArrozConLeche extends Phaser.Scene {
   this.input.keyboard.on('keydown-SPACE', () => {this.scene.start('MenuScene');});
 
   }
+  cycleRecipe(num){
+    switch(num){
+      case 1:
+        
+        //recipe.setFrame() ***to whatever image
+        //instructions.setFrame()
+        break;
+      case 2:
+      //recipe.setFrame() ***to whatever image
+        //instructions.setFrame()
+        break;
+      case 3:
+        
+        //recipe.setFrame() ***to whatever image
+        //instructions.setFrame()
+        break;
+    }
+  }
 
 
-  update(time: number, delta: number): void {
+  // update(time: number, delta: number): void {
+
+  // }
+  update() {
     
-
   }
 
 }
